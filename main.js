3c304fe804fd1706bfa6c733363d702f2f8b5cb8
manystarsapart <manystarsapart@gmail.com>
2024-07-11 15:30:36 +0800

    chore: rename learning file to accurately reflect it being experimental

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import App from './wonderhoy-app.vue'
+import App from './learning.vue'
 const app = createApp(App)
 
-// mounting to anb element with id "app"
+// mounting to an element with id "app"
 app.mount('#app')
 
 
