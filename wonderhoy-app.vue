dcbee3d684085fcd8ac54cbe06d990f5f0d8f21f
manystarsapart <manystarsapart@gmail.com>
2024-07-11 14:57:42 +0800

    feat: add todo list which user can add item to or remove item from. experimentation with v-for and list rendering

--- wonderhoy-app.vue
+++ wonderhoy-app.vue
@@ -8,12 +8,8 @@ import { reactive, ref, computed } from 'vue'
 
 // reactive is best for objects with multiple properties
 
-
-
-
 const message = ref('test')
 const counter = reactive({ count: 0, message: 'ball' })
-
 const redClass = ref('dynamicId')
 
 function increment() {
@@ -21,20 +17,37 @@ function increment() {
 }
 
 const text = ref('')
-
 const showHeading = computed(() => text.value === 'open sesame')
+
+
+let todoID = 0
+const newTodo = ref('')
+
+const todoList = ref([
+  {todoID: todoID++, text: 'test1'},
+  {todoID: todoID++, text: 'test2'},
+  {todoID: todoID++, text: 'test3'},
+  {todoID: todoID++, text: 'test4'}
+])
+
+function addTodo() {
+  todoList.value.push({todoID: todoID++, text:newTodo.value})
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todoList.value = todoList.value.filter((t) => t !== todo)
+}
 
 </script>
 
-
 <template>
-
   <div>
     {{ message }}
   </div>
 
-  <div v-bind:id=redClass>red text shown using "v-bind:id" in full </div>
-  <div :id=redClass>red text but using ":id" shorthand</div>
+  <div v-bind:id="redClass">red text shown using "v-bind:id" in full </div>
+  <div :id="redClass">red text but using ":id" shorthand</div>
 
   <hr>
 
@@ -56,6 +69,20 @@ const showHeading = computed(() => text.value === 'open sesame')
   <h1 v-if="showHeading">woah! you found the secret</h1>
   <h1 v-else>nothing to see here</h1>
   
+  <hr>
+
+    <form @submit.prevent="addTodo"> 
+      <input v-model="newTodo" required placeholder="new todo">
+      <button>add todo</button>
+    </form>
+    <ul>
+      <li v-for="todo in todoList" :key="todo.todoID">
+        {{ todo.text }}
+        <button @click="removeTodo(todo)">X</button>
+      </li>
+    </ul>
+
+
 </template>
 
 
