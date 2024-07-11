97889c336b67793f4328910ba40ccb5f5ed1e90c
manystarsapart <manystarsapart@gmail.com>
2024-07-11 12:34:08 +0800

    dev: add dynamic id and experimentation with shorthand

--- wonderhoy-app.vue
+++ wonderhoy-app.vue
@@ -1,9 +1,11 @@
 <script setup>
 
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 
 const message = ref('test')
+const counter = reactive({ count: 0 })
 
+const redClass = ref('dynamicId')
 
 </script>
 
@@ -13,14 +15,25 @@ const message = ref('test')
   <div>
     {{ message }}
   </div>
+  <p>count is {{ counter.count }}</p>
 
+  <div v-bind:id=redClass>
+    red text    
+  </div>
+
+  <div :id=redClass>
+    red text but using v-bind shorthand 
+  </div>
 
 </template>
 
 
 
-<style lang="css">
+<style>
 
-
+#dynamicId {
+  color: red;
+  
+}
 
 </style>
